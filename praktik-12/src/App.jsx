c3f369facb03d3598691ae4cda9590aa67af8527
zltwist <showeko383@gmail.com>
2025-12-16 09:53:04 +0700

    Fix vite.svg import paths for Vercel build

--- praktik-12/src/App.jsx
+++ praktik-12/src/App.jsx
@@ -1,7 +1,6 @@
 import Identity from './components/Identity';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -14,7 +13,7 @@ function App() {
 <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
