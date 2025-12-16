3d2b00aa48aa247a0e2f1f44eed4cfa1b8d93d73
zltwist <showeko383@gmail.com>
2025-12-16 12:53:16 +0700

    Add back Praktik 13 styles and improve Vercel rewrites

--- praktik-13/src/App.jsx
+++ praktik-13/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import ProductApp from './ProductApp'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <ProductApp />
+      <div className="portfolio-wrapper">
+        <ProductApp />
+      </div>
     </>
   )
 }
 
 export default App
-
